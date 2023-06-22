963f945b71f7686a8ea714a46534c26279e2adfc
master <master@SMLAB306PC24_23>
2023-06-21 21:47:25 -0500

    Agregar archivos de proyecto.

---
+++ parcial/login.xaml.cs
@@ -0,0 +1,35 @@
+﻿using parcial;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace parcial
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            string usuario = "estudiante2023";
+            string contrasena = "uisrael2023";
+            if (txtUsuario.Text == usuario && txtContraseña.Text == contrasena)
+            {
+                Navigation.PushAsync(new Page2(usuario));
+            }
+            else
+            {
+                DisplayAlert("Error", "Usuario /Contraseña incorrectos ", "Cerrar");
+            }
+        }
+    }
+}
---
+++ parcial/registro.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+
+namespace parcial
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        string usuario;
+        public Page2(string usuario)
+        {
+            InitializeComponent();
+            lblUsuario.Text = "USUARIO CONECTADO " + usuario;
+            this.usuario = usuario;
+        }
+
+
+        private void btnResumen_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page3(usuario));
+
+        }
+
+        private void btnPagoMensual_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
